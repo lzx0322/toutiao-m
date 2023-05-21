223692a7496073565ebf7837e09cf510cbe49572
lzx0322 <127064636+lzx0322@users.noreply.github.com>
2023-05-22 07:01:28 +0800

    封装请求模块

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,24 @@
+/**
+ * PostCSS 配置文件
+*/
 module.exports = {
+  // 配置要使用的 PostCSS 插件
   plugins: {
+    // 配置使用 autoprefixer 插件
+    // 作用: 生成浏览器 CSS 样式规则前缀
+    // autoprefixer: {
+    //   // autoprefixer 插件的配置
+    //   browsers: ['Android >= 4.0', 'iOS >= 8']
+    // },
     'postcss-pxtorem': {
-      rootValue: 37.5,
+      // 配置使用 postcss-pxtorem 插件
+      // 作用: 把 px 转为 rem
+      rootValue ({ file }) {
+        return file.indexOf('vant') !== -1 ? 37.5 : 75
+      },
+      // rootValue: 37.5,
+      // 配置要转换的 CSS 属性
+      // * 表示所有
       propList: ['*']
     }
   }
